62b73535357cb302aaa333643e08bf66935c98d6
Dev9tkov <idev9tkov@gmail.com>
2020-11-14 11:15:09 +0500

    Добавил объекты Subject и подписчиков для отображения задач при выборе категорий

--- src/app/service/data-handler.service.ts
+++ src/app/service/data-handler.service.ts
@@ -2,25 +2,29 @@ import { Injectable } from '@angular/core';
 import {Category} from '../model/Category';
 import {TestData} from '../data/TestData';
 import {Task} from '../model/Task';
+import {Subject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataHandlerService {
 
-  constructor() { }
+  taskSubject = new Subject<Task[]>();
+
+  constructor() {
+
+  }
 
   getCategories(): Category[] {
     return TestData.categories;
   }
 
-  getTasks(): Task[] {
-    return TestData.tasks;
+  fillTasks(): void {
+    this.taskSubject.next(TestData.tasks);
   }
 
-  getTasksByCategory(category: Category): Task[] {
+  fillTasksByCategory(category: Category): void {
     const tasks = TestData.tasks.filter(task => task.category === category);
-    console.log(tasks);
-    return tasks;
+    this.taskSubject.next(tasks);
   }
 }
